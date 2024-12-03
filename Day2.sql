c556b37ab31b1a5d02620df0a84899911422d70b
Kate Huang <102621614+katehuangishere@users.noreply.github.com>
2024-12-02 21:56:56 -0500

    Create Day2.sql

---
+++ Day2.sql
@@ -0,0 +1,17 @@
+```
+Santa wants to know which gifts weigh more than 1 kg. Can you list them?
+
+Table name: gifts
+
+gift_name	recipient	weight_kg
+Toy Train	John	2.5
+Chocolate Box	Alice	0.8
+Teddy Bear	Sophia	1.2
+Board Game	Liam	0.9
+```
+  
+SELECT *
+FROM
+    gifts
+WHERE
+    weight_kg > 1;
